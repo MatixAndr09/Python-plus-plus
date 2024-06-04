99eb1245962a0772cc8f4308aa287a29011460ee
MatixAndr09 <matixandr09@gmail.com>
2024-06-04 22:49:46 +0200

    Changed README.md, In lexer created File Reader and some CMakeLists.txt changes

--- components/lexer.cpp
+++ components/lexer.cpp
@@ -1,3 +1,37 @@
-//
-// Created by mqtio on 04.06.2024.
-//
+#include "lexer.h"
+using namespace std;
+
+map<Tokens, string> TokensMap = {
+    {Tokens::STRING, "string"},
+    {Tokens::INT, "int"},
+    {Tokens::FLOAT, "float"},
+    {Tokens::BOOL, "bool"},
+    {Tokens::CHAR, "char"},
+    {Tokens::INT_64bit, "64bit_int"},
+    {Tokens::FLOAT_64bit, "64bit_float"},
+    {Tokens::OPERATOR, "operator"},
+    {Tokens::KEYWORD, "keyword"},
+};
+
+list<string> Lexer::fileReader(const string& filename) {
+    ifstream inputFile(filename);
+    if (!inputFile.is_open()) {
+        throw runtime_error("Error opening file (Error caught in: lexer.cpp -> Lexer::fileReader)");
+    }
+
+    list<string> readedLines;
+    string line;
+    while (getline(inputFile, line)) {
+        readedLines.push_back(line);
+    }
+    inputFile.close();
+    return readedLines;
+}
+
+string Lexer::Tokenizer(const string& line) {
+
+}
+
+void Lexer::convertJSON(const string& line) {
+
+}
